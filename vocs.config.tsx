268ea379f0fd0065aacc712f4f1d592e1ccdd751
Victor Oliva <v.oliva@proton.me>
2024-04-06 22:28:36 +0200

    document structure

---
+++ vocs.config.tsx
@@ -0,0 +1,50 @@
+import { defineConfig } from "vocs";
+
+export default defineConfig({
+  title: "Polkadot-API",
+  description: "Typescript API to interact with polkadot chains",
+  sidebar: [
+    {
+      text: "Getting Started",
+      link: "/getting-started",
+    },
+    {
+      text: "Providers",
+      link: "/providers",
+    },
+    {
+      text: "Codegen",
+      link: "/codegen",
+    },
+    {
+      text: "Signers",
+      link: "/signers",
+    },
+    {
+      text: "API",
+      collapsed: true,
+      items: [
+        {
+          text: "Papee",
+          link: "/api/papee",
+        },
+      ],
+    },
+    {
+      text: "Examples",
+      collapsed: true,
+      items: [
+        {
+          text: "Example",
+          link: "/api/example",
+        },
+      ],
+    },
+  ],
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/polkadot-api/polkadot-api",
+    },
+  ],
+});
