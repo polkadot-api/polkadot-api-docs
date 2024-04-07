9a468407947ee44f3fa5e36b62923da54b7d10b3
Victor Oliva <v.oliva@proton.me>
2024-04-07 23:21:03 +0200

    add landing page

---
+++ docs/pages/KeyPoint.tsx
@@ -0,0 +1,13 @@
+import { FC, PropsWithChildren } from "react";
+
+export const KeyPoint: FC<PropsWithChildren<{ title: string }>> = ({
+  title,
+  children,
+}) => (
+  <div className="w-full border rounded border-gray-500 p-4 text-left">
+    <div className="text-[--vocs-color_heading] text-lg py-2 font-bold">
+      {title}
+    </div>
+    <div>{children}</div>
+  </div>
+);
