5201fdfe0ca7eaaf6072148b21b87ac701a63ec5
Carlo Sala <carlosalag@protonmail.com>
2025-04-14 12:52:08 +0200

    feat!: update to vocs 1.x, tailwindcss 4

--- docs/components/Tabs.tsx
+++ docs/components/Tabs.tsx
@@ -9,14 +9,14 @@ export const Root = ({
   options: Record<string, string>
 }>) => (
   <Tabs.Root
-    className="Tabs__root border rounded bg-[--vocs-color_codeBlockBackground] border-[--vocs-color_codeInlineBorder]"
+    className="Tabs__root border rounded-sm bg-(--vocs-color_codeBlockBackground) border-(--vocs-color_codeInlineBorder)"
     defaultValue={Object.keys(options)[0]}
   >
-    <Tabs.List className="Tabs__list flex flex-wrap px-2 bg-[--vocs-color_codeTitleBackground]">
+    <Tabs.List className="Tabs__list flex flex-wrap px-2 bg-(--vocs-color_codeTitleBackground)">
       {Object.entries(options).map(([value, label]) => (
         <Tabs.Trigger
           key={value}
-          className="text-sm p-3 pb-2 text-[--vocs-color_text3] border-b border-transparent hover:text-[--vocs-color_text] [&[data-state='active']]:text-[--vocs-color_text] [&[data-state='active']]:border-[--vocs-color_borderAccent]"
+          className="text-sm p-3 pb-2 text-(--vocs-color_text3) border-b border-transparent hover:text-(--vocs-color_text) data-[state='active']:text-(--vocs-color_text) data-[state='active']:font-bold data-[state='active']:border-(--vocs-color_borderAccent)"
           value={value}
         >
           {label}
