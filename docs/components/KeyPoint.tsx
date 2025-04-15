e4aaf26c0c2b6f504d21bfe33dd69a1a804a0396
Carlo Sala <carlosalag@protonmail.com>
2025-04-15 10:40:02 +0200

    feat!: update to vocs 1.x, tailwindcss 4 (#50)

--- docs/components/KeyPoint.tsx
+++ docs/components/KeyPoint.tsx
@@ -4,8 +4,8 @@ export const KeyPoint: FC<PropsWithChildren<{ title: string }>> = ({
   title,
   children,
 }) => (
-  <div className="w-full border rounded border-gray-500 p-4 text-left">
-    <div className="text-[--vocs-color_heading] text-lg py-2 font-bold">
+  <div className="w-full border rounded-sm border-gray-500 p-4 text-left">
+    <div className="text-(--vocs-color_heading) text-lg py-2 font-bold">
       {title}
     </div>
     <div>{children}</div>
